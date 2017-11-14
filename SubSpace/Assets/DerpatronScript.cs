3a912958d0775a562957cb9f26695f2991af88aa
Sondrex <sonwrop@gmail.com>
2017-11-14 19:32:27 +0100

    Added begynnelsen på mob 2

---
+++ SubSpace/Assets/DerpatronScript.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerpatronScript : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody2D derpatron;
+    public Animator derpAnimation;
+    public float speed;
+    public bool shooting;
+    public bool right;
+    float counter; // DEBUG 
+
+    // TODO:
+    /*
+     * Spawn projectiles when mob is attacking
+     * Make checks fo player be not when the player impacts the sircle, but when they stay there
+     * Make checktime longer
+     * Make projectile(currently an empty shell)
+     * 
+     * 
+    */
+    void Start () {
+        derpatron = GetComponent<Rigidbody2D>();
+        derpAnimation = GetComponent<Animator>();
+        setSpeed();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        AI();
+    }
+
+    void AI() // sets speed or attacks
+    {
+        if (shooting)
+            Attack();
+        else
+            setSpeed();
+
+    }
+    void setSpeed()
+    {
+        if (derpatron.velocity.x == 0 /*derpatron.velocity.x >= -0.1 && derpatron.velocity.x <= 0.1*/) // Checks if it is standing still, and triggers the needed animation
+        {
+            derpAnimation.SetBool("standingStill", true);
+            if (counter == 0)
+            {
+                right = !right;
+                counter = 150;
+                Debug.Log(right);
+            }
+            else
+                counter--;
+        }
+        else
+            derpAnimation.SetBool("standingStill", false);
+
+        if (right)
+        {
+            if (derpatron.velocity.x < speed)
+            {
+                derpatron.velocity += new Vector2(speed, 0f);
+                flip(true);
+            }
+        }
+        else // moving right
+        {
+            if (derpatron.velocity.x > -speed)
+            {
+                derpatron.velocity += new Vector2(-speed, 0f);
+                flip(false);
+            }
+        }
+        
+
+        derpAnimation.SetFloat("currentSpeed", derpatron.velocity.x * Time.deltaTime); // Updates speed bool for animations
+    }
+    void flip(bool flipRight)
+    {
+        GameObject Derp_enemy = GameObject.Find("Mob2_derpatron");
+
+        
+
+        if (flipRight)  // right
+                Derp_enemy.transform.localScale = new Vector3(1, 1, 1);
+        else                           // left
+            Derp_enemy.transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void Attack()
+    {
+        derpAnimation.SetBool("standingStill", true);
+        derpatron.velocity = new Vector2(0, derpatron.velocity.y); // makes the mob stand still when detecting the player
+        derpAnimation.SetBool("isShooting", true);
+        counter += Time.deltaTime;
+        if (counter > 50000 * Time.deltaTime) // timer so it stops attacking after a little while
+        {
+            derpAnimation.SetBool("isShooting", false);
+            shooting = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            Debug.Log("Player touched the deeeerp");
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shooting = true;
+
+            Debug.Log("Player!");
+
+            if (right && other.transform.position.x < transform.position.x) // checks if the player is to the right of the mob
+            {
+                flip(true);
+            }
+        }
+    }
+}
---
+++ SubSpace/Assets/Derpatron_projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Derpatron_projectile : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D projetile;
+
+	// Use this for initialization
+	void Start () {
+        projetile = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        projetile.MoveRotation(speed* Time.deltaTime);
+
+
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            Debug.Log("Player touched projectile, take damage!!");
+        if (coll.gameObject.tag == "Wall" || coll.gameObject.tag == "Roof" || coll.gameObject.tag == "floor" || coll.gameObject.tag == "Platform" || coll.gameObject.tag == "floor" || coll.gameObject.tag == "Player")
+            Destroy(gameObject);
+    }
+}
