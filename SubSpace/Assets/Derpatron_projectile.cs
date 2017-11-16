19f97e84d18fff8593e80ddd7881af45d5f98ffd
TheKiwiGivi <33149624+TheKiwiGivi@users.noreply.github.com>
2017-11-16 14:49:07 +0100

    hp og checkpoint
    lul

--- SubSpace/Assets/Derpatron_projectile.cs
+++ SubSpace/Assets/Derpatron_projectile.cs
@@ -27,8 +27,13 @@ public class Derpatron_projectile : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D coll) // kan hende bruke OnTriggerEnter2D vil fungere, men for øyeblikket triggerer detectboxen den
     {
         if (coll.gameObject.tag == "Player")
+        {
             Debug.Log("Player touched projectile, take damage!!");
-        if (coll.gameObject.tag != "Enemy" && coll.gameObject.tag != "PlayerDetection" && coll.gameObject.tag != "Projectile") // despawns object when it hits something that isn't an enemy
+            GameObject.Find("player").GetComponent<PlayerMovement>().playerHealth--;
+        }
+            
+        if (coll.gameObject.tag != "Enemy" && coll.gameObject.tag != "PlayerDetection" && coll.gameObject.tag != "Projectile"
+            && coll.gameObject.tag != "WallDetection" && !coll.CompareTag("Checkpoint")) // despawns object when it hits something that isn't an enemy
             Destroy(gameObject);
     }
 }
---
+++ SubSpace/Assets/Standard Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour {
+
+    public int numberInMap;
+    public bool playerPassed = false;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !playerPassed)
+        {
+            GameObject.Find("player").GetComponent<PlayerMovement>().currentCheckpoint = transform.position;
+            playerPassed = true;
+        }
+    }
+}
