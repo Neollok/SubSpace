8d7273fabf762c5ded60497dcca14de61477847d
iamartn <33090221+iamartn@users.noreply.github.com>
2017-11-30 12:34:39 +0100

    grg

--- SubSpace/Assets/Standard Assets/Scripts/PlayerShoot.cs
+++ SubSpace/Assets/Standard Assets/Scripts/PlayerShoot.cs
@@ -34,79 +34,76 @@ public class PlayerShoot : MonoBehaviour
 
     void Update()
     {
+        float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+        crosshair.transform.position = new Vector2(mouseX, mouseY);
+       
 
+        crosshair.transform.localScale = new Vector2(.3f+(bulletSpread/10), .3f + (bulletSpread/10));
 
-        
-            float mouseX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-            float mouseY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
-            crosshair.transform.position = new Vector2(mouseX, mouseY);
+        if(bulletSpread > 0 && !Input.GetButton("Fire1"))
+            bulletSpread -= 16 * Time.deltaTime;
+        if(bulletSpread < 0)
+            bulletSpread = 0;
 
+        if(gunCooldown > 0)
+        {
+            gunCooldown -= 1 * Time.deltaTime;
+        }
 
-            crosshair.transform.localScale = new Vector2(.3f + (bulletSpread / 10), .3f + (bulletSpread / 10));
-
-            if (bulletSpread > 0 && !Input.GetButton("Fire1"))
-                bulletSpread -= 16 * Time.deltaTime;
-            if (bulletSpread < 0)
-                bulletSpread = 0;
-
-            if (gunCooldown > 0)
+        if(gunCooldown <= 0)                           //If no cooldown for shooting
+        {
+            if(Input.GetButton("Fire1"))        //Left mouse button
             {
-                gunCooldown -= 1 * Time.deltaTime;
-            }
-
-            if (gunCooldown <= 0)                           //If no cooldown for shooting
-            {
-                if (Input.GetButton("Fire1"))        //Left mouse button
-                {
                     Shoot();
                     gunCooldown = shootCD;
-                    if (bulletSpread < bulletSpreadMax)
-                        bulletSpread += bulletSpreadIncrease;
-                }
+                    if(bulletSpread < bulletSpreadMax)
+                    bulletSpread += bulletSpreadIncrease;
             }
-
         }
 
+    }
 
 
-        void Shoot()
+
+    void Shoot()
+    {
+
+        Vector2 mousePos = new Vector2(
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);     //Mouse position in game in a vector2
+                                                                      
+
+        Vector2 firePointPos = new Vector2(
+            firePoint.position.x, 
+            firePoint.position.y);   //Position of firing point also in a vector2
+
+        //If the mouse X position - the player position is less than 0
+        if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - GameObject.Find("player").transform.position.x < 0)
         {
-
-            Vector2 mousePos = new Vector2(
-                Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
-                Camera.main.ScreenToWorldPoint(Input.mousePosition).y);     //Mouse position in game in a vector2
-
-
-            Vector2 firePointPos = new Vector2(
-                firePoint.position.x,
-                firePoint.position.y);   //Position of firing point also in a vector2
-
-            //If the mouse X position - the player position is less than 0
-            if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - GameObject.Find("player").transform.position.x < 0)
-            {
-
-                //Then we want the bullets to move the opposite direction as well
-            }
-
-            //This ray is a straight invisible beam from fire position to (mouse position-fire position)
-            //100 is the distance (not important). whatToHit is what layers to interact with
-            RaycastHit2D hit = Physics2D.Raycast(firePointPos, mousePos - firePointPos, 100, whatNotToHit);
-
-
-            Effect();               //Runs the function that creates an instance of a bullet
-
-
-
-
-            Debug.DrawLine(firePointPos, (mousePos - firePointPos) * 100);     //Used for debugging. can see the bullets ray
-            if (hit.collider != null)
-            {
-                Debug.DrawLine(firePointPos, hit.point, Color.red);
-            }
+          
+                        //Then we want the bullets to move the opposite direction as well
         }
-        void Effect()
+
+        //This ray is a straight invisible beam from fire position to (mouse position-fire position)
+        //100 is the distance (not important). whatToHit is what layers to interact with
+        RaycastHit2D hit = Physics2D.Raycast(firePointPos, mousePos - firePointPos, 100, whatNotToHit);
+
+        
+        Effect();               //Runs the function that creates an instance of a bullet
+            
+        
+                                         
+
+        Debug.DrawLine(firePointPos, (mousePos-firePointPos)*100);     //Used for debugging. can see the bullets ray
+        if(hit.collider != null)
         {
-            Transform bullet = (Transform)Instantiate(bulletTrailPrefab, firePoint.position, transform.rotation * Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + Random.Range(-bulletSpread, bulletSpread))); //Instantiates of the prefab to the actual game   
+            Debug.DrawLine(firePointPos, hit.point, Color.red);
         }
-    
+    }
+    void Effect()
+    {
+        Transform bullet = (Transform)Instantiate(bulletTrailPrefab, firePoint.position, transform.rotation * Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + Random.Range(-bulletSpread, bulletSpread))); //Instantiates of the prefab to the actual game   
+        
+    }
 }
