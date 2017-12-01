30af51b6913279fbd54875c4e666a15bd4808419
iamartn <33090221+iamartn@users.noreply.github.com>
2017-12-01 14:38:23 +0100

    Made a box that loads a new level

---
+++ SubSpace/Assets/Scripts/LoadLevelBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelBox: MonoBehaviour
+{
+
+    bool withinRange = false;
+
+    public string LevelToLoad;
+
+    void Update()
+    {
+        if (withinRange)
+        {
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene(LevelToLoad);
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player" || other.transform.tag == "GroundCheck")
+            withinRange = true;
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player")
+            withinRange = false;
+
+    }
+}
+
