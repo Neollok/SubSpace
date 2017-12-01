a67b5733e20f7fa684b8f2ca518618b7ce98099c
Sondrex <sonwrop@gmail.com>
2017-12-01 14:44:58 +0100

    Projectile should work now with boss

--- SubSpace/Assets/UnfallingProjectile.cs
+++ SubSpace/Assets/UnfallingProjectile.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnfallingProjectile : MonoBehaviour {
-    void OnTriggerEnter2D(Collider2D coll) 
+public class UnfallingProjectile : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
             Debug.Log("Player touched projectile, take damage!!");
-            if(GameObject.Find("player").GetComponent<PlayerMovement>().loopNotHurtRunning == false)
-           GameObject.Find("player").GetComponent<PlayerMovement>().playerHealth--;
+            if (GameObject.Find("player").GetComponent<PlayerMovement>().loopNotHurtRunning == false)
+                GameObject.Find("player").GetComponent<PlayerMovement>().playerHealth--;
         }
 
-        if (coll.gameObject.tag != "Enemy" && coll.gameObject.tag != "Boss" && coll.gameObject.tag != "PlayerDetection" && coll.gameObject.tag != "Projectile"
-            && coll.gameObject.tag != "WallDetection" && !coll.CompareTag("Checkpoint") && coll.CompareTag("BossBox") && !coll.CompareTag("Shots")) // despawns object when it hits something that isn't an enemy
+        if (coll.gameObject.tag != "Enemy" && coll.gameObject.tag != "Boss" && coll.gameObject.tag != "PlayerDetection" && coll.gameObject.tag != "Projectile" && coll.gameObject.tag != "BossBox"
+            && coll.gameObject.tag != "WallDetection" && !coll.CompareTag("Checkpoint") && !coll.CompareTag("Shots")) // despawns object when it hits something that isn't an enemy
+        {
             Destroy(gameObject);
+            Debug.Log("Destroy!!!");
+        }
     }
 }
