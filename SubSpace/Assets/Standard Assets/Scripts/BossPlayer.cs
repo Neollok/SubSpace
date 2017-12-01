9f5d409fdda05dc0fd56e9f747037e9585979612
TheKiwiGivi <33149624+TheKiwiGivi@users.noreply.github.com>
2017-12-01 14:51:18 +0100

    boss posisjonfiks

---
+++ SubSpace/Assets/Standard Assets/Scripts/BossPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPlayer : MonoBehaviour {
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameObject.Find("BossMob1").GetComponent<BossScript>().posY = other.transform.position.y - transform.position.y;
+        }
+    }
+}
